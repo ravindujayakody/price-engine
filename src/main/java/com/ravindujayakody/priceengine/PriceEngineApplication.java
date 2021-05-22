6065f05ff81d8c39e217a51adc3b485e88d6af9c
Ravindu <ravindu@edhirya.com>
2021-05-23 01:36:25 +0530

    gradle to maven

--- src/main/java/com/ravindujayakody/priceengine/PriceEngineApplication.java
+++ src/main/java/com/ravindujayakody/priceengine/PriceEngineApplication.java
@@ -2,12 +2,18 @@ package com.ravindujayakody.priceengine;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class PriceEngineApplication {
+public class PriceEngineApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PriceEngineApplication.class, args);
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return super.configure(builder);
+	}
 }
