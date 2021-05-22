d35269d450ae3517abf643b768328ca0373a4bc7
Ravindu <ravindu@edhirya.com>
2021-05-22 14:44:26 +0530

    base

---
+++ src/test/java/com/ravindujayakody/priceengine/PriceEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ravindujayakody.priceengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PriceEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
